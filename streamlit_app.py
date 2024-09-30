daeb08742531dd2a7fd68e05379d502067617bca
johnzelson <johnzelson@gmail.com>
2024-09-30 13:35:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,8 +109,8 @@ def people_by_emphasis_graph(df):
     # Build force-directed graph with default settings
     d3.graph(adjmat)
     #d3.show(filepath='data/g1.html')
-    #d3.show()
-    d3.show(filepath='data/ppl_emph.html')
+    d3.show()
+    #d3.show(filepath='data/ppl_emph.html')
 
 
 def main():

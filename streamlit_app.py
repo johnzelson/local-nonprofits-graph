4a22d611a1f991c86d21042a151b23717aa1d75d
johnzelson <johnzelson@gmail.com>
2024-09-30 07:13:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,5 @@
 import streamlit as st
 from streamlit_d3graph import d3graph
-import pandas as pd
-from d3graph import d3graph, vec2adjmat
-
-
-def get_people_df():
-
-    all_people_df = pd.read_csv('data/all_people.csv')
-    return all_people_df
 
 
 @st.cache_data
@@ -15,25 +7,10 @@ def init_graph():
     # Initialize
     d3 = d3graph()
     # Load karate example
-    #adjmat, df = d3.import_example("karate")
+    adjmat, df = d3.import_example("karate")
 
-    df = get_people_df()
-    source= []
-    target = []
-    weight = []
-
-    for index, row in df.iterrows():
-        source.append(row['NAME'])
-        target.append(row['PersonNm'])
-        weight.append(1)
-        
-    # Create adjacency matrix
-    adjmat = vec2adjmat(source, target, weight=weight)
-
-
-    label = df["NAME"].values
-    # node_size = df["PersonNm"].values
-    node_size = 1
+    label = df["label"].values
+    node_size = df["degree"].values
 
     return d3, adjmat, df, label, node_size
 
@@ -41,7 +18,7 @@ def init_graph():
 @st.cache_data
 def graph_one(_d3, adjmat, df, label, node_size):
     d3.graph(adjmat)
-    #d3.set_node_properties(color=df["label"].values)
+    d3.set_node_properties(color=df["label"].values)
     return d3
 
 
@@ -101,7 +78,6 @@ d3, adjmat, df, label, node_size = init_graph()
 d3 = graph_one(d3, adjmat, df, label, node_size)
 d3.show()
 
-"""
 d3 = graph_two(d3, adjmat, df, label, node_size)
 d3.show()
 
@@ -119,5 +95,3 @@ d3.show()
 
 d3 = graph_seven(adjmat, df, label, node_size)
 d3.show()
-
-"""
